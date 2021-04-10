939e9be62d785a547c5c2aecac938ccc0a79cf44
Mikael Ruonala <Mikael.Ruonala@Metropolia.fi>
2021-04-10 11:03:06 +0300

    'auth'

--- server.js
+++ server.js
@@ -6,18 +6,27 @@ import resolvers from './resolvers/index.js';
 import express from 'express';
 import connectDB from './database/db.js'
 import dotenv from 'dotenv';
+import {checkAuth} from "./passport/authenticate.js";
 
 dotenv.config();
 
 (async () => {
     try {
         const con = await connectDB();
-        if(con){
+        if (con) {
             console.log('connection to db succesful');
         }
         const server = new ApolloServer({
             typeDefs: schemas,
             resolvers: resolvers,
+            context: async ({req, res}) => {
+                try {
+                    const user = await checkAuth(req, res);
+                    return {req, res, user};
+                } catch (error) {
+                    console.log('context error:', error);
+                }
+            }
         });
         const app = express();
 
